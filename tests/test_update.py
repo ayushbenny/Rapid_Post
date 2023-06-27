33702dfcc8c3554d60592b613f7ad2d8601a9f09
ayush <ayush@lanwaresolutions.com>
2023-06-27 12:57:23 +0530

    added the Update funcitonality as both PUT and PATCH

--- tests/test_update.py
+++ tests/test_update.py
@@ -1,6 +1,9 @@
 from rapid_post_folder import RapidUpdate
 
 
+"""Need to provide all the columns and the values for PUT method annd in the \
+PATCH method, just need to pass only the required field"""
+
 data_id = {"id": 4}
 table_name = {"table_name": "test"}
 payload = {
